7f5674c7899374707f4e63d2364560c67f09e2d5
Dami908 <55024367+Dami908@users.noreply.github.com>
2021-03-16 22:05:49 -0400

    Added code changes

--- MechanicChecker/MechanicChecker/Controllers/HomeController.cs
+++ MechanicChecker/MechanicChecker/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace MechanicChecker.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page. new changes";
 
             return View();
         }

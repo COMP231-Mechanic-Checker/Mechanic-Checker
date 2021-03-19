695200d80c5f9ded9e126efc445c53416b356580
FatemaKhatun62124 <71528692+FatemaKhatun62124@users.noreply.github.com>
2021-03-19 11:32:05 -0400

    Code functionality
    Filter returned  products based on filter options

---
+++ MechanicChecker/MechanicChecker/Models/LocalProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace MechanicChecker.Models
+{
+    public class LocalProduct
+    {
+        private LocalProductContext context;
+        [Key]
+        public int LocalProductId { get; set; }
+        //[Required(ErrorMessage = "Please enter your name")]
+        public string Title { get; set; }
+        //[Required(ErrorMessage = "Please choose the amount of servings")]
+        public string Price { get; set; }
+        //[Required(ErrorMessage = "Please enter your dish name")]
+        public string Description { get; set; }
+       // [Required(ErrorMessage = "Please enter the description of your dish")]
+        public string ImageUrl { get; set; }
+        //[Required(ErrorMessage = "Please enter the Instructions of your dish")]
+        public bool IsVisible { get; set; }
+        public string Category { get; set; }
+        public string ProductUrl { get; set; }
+        public bool IsQuote { get; set; }
+        public string sellerId { get; set; }
+    }
+}

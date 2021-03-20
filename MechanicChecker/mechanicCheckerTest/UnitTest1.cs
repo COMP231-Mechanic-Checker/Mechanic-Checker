6336eea28573cb5cb0448ff8f35dd04bd6151d2c
Dami908 <55024367+Dami908@users.noreply.github.com>
2021-03-19 23:16:01 -0400

    Implement testing scripts to satisfy the acceptance criteria #17

---
+++ MechanicChecker/mechanicCheckerTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace mechanicCheckerTest
+{
+    public class Tests
+    {
+       
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

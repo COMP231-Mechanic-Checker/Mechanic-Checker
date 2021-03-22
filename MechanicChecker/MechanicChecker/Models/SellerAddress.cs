a545ac4795a164e0c7a888ef806d9c59c72d7fd2
Shaminda <shaminda1017@gmail.com>
2021-03-22 12:11:21 -0400

    Added Geo location to find distance

---
+++ MechanicChecker/MechanicChecker/Models/SellerAddress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MechanicChecker.Models
+{
+     public class SellerAddress
+     {
+        public int AddressId { get; set; }
+        public int SellerId { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+    }
+}

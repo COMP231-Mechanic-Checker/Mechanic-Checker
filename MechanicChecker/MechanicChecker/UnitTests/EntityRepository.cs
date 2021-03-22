cd97f588c7df835506d28ed8599c9186e6b39a55
Dami908 <55024367+Dami908@users.noreply.github.com>
2021-03-22 14:59:43 -0400

    Testing Code using nunit

---
+++ MechanicChecker/MechanicChecker/UnitTests/EntityRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MechanicChecker.UnitTests
+{
+    public class EntityRepository<T> : IEntityRepository<T> where T : class, new()
+    {
+        private LocalProductDbContext _dbcontext;
+        private DbSet<T> _dbset;
+
+        public EntityRepository(LocalProductDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+            _dbset = _dbcontext.Set<T>();
+        }
+        public IQueryable<T> GetAllQueryAble()
+        {
+            return _dbset;
+        }
+
+        public void Insert(T entity)
+        {
+            _dbset.Add(entity);
+            _dbcontext.SaveChanges();
+        }
+    }
+
+}

354100f9a24002678cd58316319833bae90f69d7
Dami908 <55024367+Dami908@users.noreply.github.com>
2021-03-22 15:01:01 -0400

    Revert "Testing Code using nunit"
    This reverts commit cd97f588c7df835506d28ed8599c9186e6b39a55.

--- MechanicChecker/mechanicCheckerTest/TestMethods/localProductTests.cs
+++ MechanicChecker/mechanicCheckerTest/TestMethods/localProductTests.cs
@@ -1,41 +1,38 @@
 ﻿using MechanicChecker.Models;
-using MechanicChecker.UnitTests;
 using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace mechanicCheckerTest.TestMethods
 {
     public class localProductTests
     {
-        private Mock<IEntityRepository<LocalProduct>> productLocal;
-        private List<LocalProduct> product;
-        [SetUp]
-        public void Setup()
-        {
-            productLocal = new Mock<IEntityRepository<LocalProduct>>();
-            product = new List<LocalProduct>();
-            product.Add(new LocalProduct() { LocalProductId=1,Title="Tires",Price="79",ImageUrl="url",IsVisible=false,ProductUrl="url",IsQuote=true,sellerId="5",Category="Car parts",Description="This is a tire"});//Add table details
+        //private Mock<IEntityRepository<LocalProduct>> productLocal;
+        //private List<LocalProduct> product;
+        //[SetUp]
+        //public void Setup()
+        //{
+        //    productLocal = new Mock<IEntityRepository<LocalProduct>>();
+        //    product = new List<LocalProduct>();
+        //    product.Add(new LocalProduct() { });//Add table details
 
-        }
+        //}
 
-        [Test]
-        public void Test1()
-        {
-            //Act
-            productLocal.Setup(a => a.GetAllQueryAble()).Returns(product.AsQueryable());
+        //[Test]
+        //public void Test1()
+        //{
+        //    //Act
+        //    productLocal.Setup(a => a.GetAllQueryable()).Returns(product.AsQueryable());
 
-            //Arrange
-            var bl = new LocalProductBL(productLocal.Object); // change to the class similar to BL
-            var List = bl.GetAllActiveProduts();
+        //    //Arrange
+        //    var bl = new BL(productLocal.Object); // change to the class similar to BL
+        //    var List = bl.getactiveproducts();
 
 
-           
-            Assert.IsTrue(List.Count == 2);
-            Assert.IsTrue(List.All(S => S.IsVisible == false));
-        }
+        //    Assert.Pass();
+        //    Assert.IsTrue(List.Count == 2);
+        //}
     }
 }

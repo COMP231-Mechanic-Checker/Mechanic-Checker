8483afae7d8334e321b9ae31fa54c5239479e07e
Shaminda <shaminda1017@gmail.com>
2021-04-05 19:32:07 -0400

    Added big box  retailers API function

---
+++ MechanicChecker/MechanicChecker/Models/ExternalAPIs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MechanicChecker.Models
+{
+    public class ExternalAPIs
+    {
+        private ExternalAPIsContext context;
+        [Key]
+
+        public int APIKeyId { get; set; }
+        public string Service { get; set; }
+        public string APIKey { get; set; }
+        public string KeyOwner { get; set; }
+        public int Quota { get; set; }
+        public DateTime ActiveDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public string APIHost {get; set;}
+
+    }
+}

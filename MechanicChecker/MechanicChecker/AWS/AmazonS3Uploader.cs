08c1176b76fa1b8a83ea64ff26dc524377beed0d
Dami908 <55024367+Dami908@users.noreply.github.com>
2021-04-07 20:58:56 -0400

    Added S3 Upload class and packages
    Issue #54
    Estimated time 2hrs

---
+++ MechanicChecker/MechanicChecker/AWS/AmazonS3Uploader.cs
@@ -0,0 +1,57 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MechanicChecker
+{
+    public class AmazonS3Uploader
+    {
+        public static string folder = "seller";
+        private string bucketName = "mechanic.checker";
+        // private string keyName = string.Format("{0}/{1}", folder, filename);
+        //private string filePath = "C:\\Users\\EMMAL\\Documents\\Laptop Stuff\\Pictures\\b.jpg";
+
+        public void UploadFile(string filepath, string filename)
+        {
+            string keyName = string.Format("{0}/{1}", folder, filename);
+            var client = new AmazonS3Client(Amazon.RegionEndpoint.USEast1);
+            var transferUtility = new TransferUtility(client);
+
+            try
+            {
+                TransferUtilityUploadRequest transferUtilityUploadRequest = new TransferUtilityUploadRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName,
+                    FilePath = filepath
+                };
+                transferUtility.Upload(transferUtilityUploadRequest);
+                GetPreSignedUrlRequest request = new GetPreSignedUrlRequest();
+                request.BucketName = bucketName;
+                request.Key = keyName;
+                request.Expires = DateTime.Now.AddHours(11);
+                request.Protocol = Protocol.HTTP;
+                string url = client.GetPreSignedURL(request);
+                Console.WriteLine(url);
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null &&
+                    (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId")
+                    ||
+                    amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                {
+                    throw new Exception("Check the provided AWS Credentials.");
+                }
+                else
+                {
+                    throw new Exception("Error occurred: " + amazonS3Exception.Message);
+                }
+            }
+        }
+    }
+}
---
+++ MechanicChecker/MechanicChecker/AWS/S3FileUploader.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MechanicChecker
+{
+    public class S3FileUploader
+    {
+        public string value(IFormFile files)
+        {
+            //var size = files.Sum(f => f.Length);
+            var filePaths = new List<string>();
+            string filepath;
+            string v;
+            Random rnd = new Random();
+            int number = rnd.Next(1, 10);
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            char[] stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+
+            string filename = string.Format("{0},{1}", finalString, files.FileName);
+            //string encodedStr = Convert.ToBase64String(Encoding.UTF8.GetBytes(filename));
+            filepath = Path.Combine(Directory.GetCurrentDirectory(), files.FileName);
+            string path = Path.GetFileName(files.FileName);
+            Console.WriteLine(filepath);
+            filePaths.Add(filepath);
+            //amazonS3.UploadFile(filepath, formFile.FileName);
+            //using (var stream = new FileStream(filepath, FileMode.Create))
+            //{
+            //    await formFile.CopyToAsync(stream);
+            //}
+            AmazonS3Uploader amazonS3 = new AmazonS3Uploader();
+            amazonS3.UploadFile(filepath, finalString);
+            v = "https://s3.amazonaws.com/mechanic.checker/seller/" + finalString;
+            //sql code
+
+            Console.Write(v);
+            return v;
+
+        }
+       
+    }
+}

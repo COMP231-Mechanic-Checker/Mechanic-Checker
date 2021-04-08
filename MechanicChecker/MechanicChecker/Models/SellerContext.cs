c8cfbd949c0f2a7900ba295b0f14afcb752bb116
Ibrahim Jomaa <ibrahimj789@gmail.com>
2021-04-08 13:11:25 -0400

    Add sendgrid reset password functionality

--- MechanicChecker/MechanicChecker/Models/SellerContext.cs
+++ MechanicChecker/MechanicChecker/Models/SellerContext.cs
@@ -81,19 +81,94 @@ namespace MechanicChecker.Models
                 isPassed = reader.Read() && (Convert.ToBoolean(reader["IsApproved"]) == false);
             }
 
-            if (isPassed.Equals(true))
+            if (isPassed)
             {
                 string stringCmd = "UPDATE Seller SET IsApproved = 1, ApprovalDate = '" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "' WHERE ActivationCode = '" + activationId + "';";
 
                 MySqlCommand secondCommand = new MySqlCommand(stringCmd);
                 secondCommand.Connection = myConnection;
                 secondCommand.ExecuteNonQuery(); // ExecuteNonQuery is required to update, insert and delete from the DB 
-
             }
             myConnection.Close();
 
             return isPassed;
 
+        }
+        public bool verifyUserByEmail(string email, out string sellerName)
+        {
+            bool isPassed = false;
+
+            string command = "SELECT Username, Email from Seller where Email = '" + email + "';";
+
+            // send query to database
+            MySqlConnection myConnection = GetConnection();
+            MySqlCommand myCommand = new MySqlCommand(command);
+            myCommand.Connection = myConnection;
+            myConnection.Open();
+
+            // read response
+            using (var reader = myCommand.ExecuteReader())
+            {
+                // will only run if the query returns a record
+                isPassed = reader.Read();
+                if (isPassed)
+                {
+                    sellerName = reader["Username"].ToString();
+                }
+                else
+                {
+                    sellerName = null;
+                }
+                
+            }
+
+            return isPassed;
+        }
+
+        public bool updatePassword(string password, string resetPasswordCode)
+        {
+            bool isPassed = false;
+
+            string command = "UPDATE Seller SET PasswordHash = '" + password + "' WHERE ResetPasswordCode = '" + resetPasswordCode + "';";
+
+            // send query to database
+            MySqlConnection myConnection = GetConnection();
+            MySqlCommand myCommand = new MySqlCommand(command);
+            myCommand.Connection = myConnection;
+            myConnection.Open();
+            isPassed = Convert.ToBoolean(myCommand.ExecuteNonQuery()); // only 1 row should be affected; if failed then 0 rows were affected
+
+            // remove ResetPasswordCode from DB since its not needed anymore
+            // also prevents anyone from resetting the password with the same link again
+            if (isPassed)
+            {
+                myCommand.CommandText = "UPDATE Seller SET ResetPasswordCode = " + "NULL" + " WHERE ResetPasswordCode = '" + resetPasswordCode + "';";
+                isPassed = Convert.ToBoolean(myCommand.ExecuteNonQuery()); // only 1 row should be affected; if failed then 0 rows were affected
+                myCommand.Connection.Close();
+            }
+            else
+            {
+                myCommand.Connection.Close();
+            }
+
+            return isPassed;
+        }
+
+        public bool updateResetPasswordCode(string resetPasswordCode, string sellerEmail)
+        {
+            bool isPassed = false;
+
+            string command = "UPDATE Seller SET ResetPasswordCode = '" + resetPasswordCode + "' WHERE Email = '" + sellerEmail + "';";
+
+            // send query to database
+            MySqlConnection myConnection = GetConnection();
+            MySqlCommand myCommand = new MySqlCommand(command);
+            myCommand.Connection = myConnection;
+            myConnection.Open();
+            isPassed = Convert.ToBoolean(myCommand.ExecuteNonQuery()); // only 1 row should be affected; if failed then 0 rows were affected
+            myCommand.Connection.Close();
+
+            return isPassed;
         }
 
     }

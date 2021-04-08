d38b481a210de97df01610d3dccfec5e0a6890b4
Ibrahim Jomaa <ibrahimj789@gmail.com>
2021-04-08 00:01:59 -0400

    Modify s3 upload function to work with filestream

--- MechanicChecker/MechanicChecker/AWS/S3FileUploader.cs
+++ MechanicChecker/MechanicChecker/AWS/S3FileUploader.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,16 +9,12 @@ namespace MechanicChecker
 {
     public class S3FileUploader
     {
-        public string value(IFormFile files)
+        private int filenameLength = 50;
+        private string awsS3BucketUrl = "https://s3.amazonaws.com/mechanic.checker/seller/";
+        public string value(IFormFile file)
         {
-            //var size = files.Sum(f => f.Length);
-            var filePaths = new List<string>();
-            string filepath;
-            string v;
-            Random rnd = new Random();
-            int number = rnd.Next(1, 10);
             string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            char[] stringChars = new char[8];
+            char[] stringChars = new char[filenameLength];
             var random = new Random();
 
             for (int i = 0; i < stringChars.Length; i++)
@@ -26,26 +22,13 @@ namespace MechanicChecker
                 stringChars[i] = chars[random.Next(chars.Length)];
             }
 
-            var finalString = new String(stringChars);
+            var finalString = new String(stringChars) + Path.GetExtension(file.FileName);
 
-            string filename = string.Format("{0},{1}", finalString, files.FileName);
-            //string encodedStr = Convert.ToBase64String(Encoding.UTF8.GetBytes(filename));
-            filepath = Path.Combine(Directory.GetCurrentDirectory(), files.FileName);
-            string path = Path.GetFileName(files.FileName);
-            Console.WriteLine(filepath);
-            filePaths.Add(filepath);
-            //amazonS3.UploadFile(filepath, formFile.FileName);
-            //using (var stream = new FileStream(filepath, FileMode.Create))
-            //{
-            //    await formFile.CopyToAsync(stream);
-            //}
             AmazonS3Uploader amazonS3 = new AmazonS3Uploader();
-            amazonS3.UploadFile(filepath, finalString);
-            v = "https://s3.amazonaws.com/mechanic.checker/seller/" + finalString;
-            //sql code
+            amazonS3.UploadFile(finalString, file);
+            string awsS3ImageUrl = awsS3BucketUrl + finalString;
 
-            Console.Write(v);
-            return v;
+            return awsS3ImageUrl;
 
         }
        

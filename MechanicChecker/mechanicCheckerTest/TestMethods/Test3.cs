cb6031ac724dbe3502d0075570aa7358612f3476
Dami908 <55024367+Dami908@users.noreply.github.com>
2021-04-15 17:57:04 -0400

    Added unit testing Test cases

---
+++ MechanicChecker/mechanicCheckerTest/TestMethods/Test3.cs
@@ -0,0 +1,20 @@
+﻿using MechanicChecker.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mechanicCheckerTest.TestMethods
+{
+    class Test3
+    {
+        [Test]
+        public void Test1()
+        {
+            var controller = new AccountController();
+            var result = controller.SignIn() as ViewResult;
+            Assert.AreEqual("SignIn", result.ViewName);
+        }
+    }
+}

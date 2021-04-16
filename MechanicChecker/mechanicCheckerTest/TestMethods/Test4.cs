851c7ade97e58fcee30ff5198f40c931211e9f3d
Dami908 <55024367+Dami908@users.noreply.github.com>
2021-04-15 22:43:39 -0400

    Added use tests

---
+++ MechanicChecker/mechanicCheckerTest/TestMethods/Test4.cs
@@ -0,0 +1,27 @@
+﻿using MechanicChecker.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mechanicCheckerTest.TestMethods
+{
+    class Test4
+    {
+        //Unit test to see if the SearchCompareTwo returns a View
+        [Test]
+        public void Employees()
+        {
+            // Arrange
+            SearchController controller = new SearchController();
+
+            // Act
+            ViewResult result = controller.SearchCompareTwoParts() as ViewResult;
+
+            // Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ MechanicChecker/mechanicCheckerTest/TestMethods/Test5.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using MechanicChecker.Controllers;
+using Assert = NUnit.Framework.Assert;
+
+namespace mechanicCheckerTest.TestMethods
+{
+    class Test5
+    {
+        [Test]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
